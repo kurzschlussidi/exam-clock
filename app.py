925ef614f3670ed4d544760b2ee3e682f3cd194e
kurzschlussidi <you@example.com>
2021-03-27 20:30:32 +0100

    adding the option to show remaining time

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, url_for, render_template, send_from_
 from datetime import datetime, timedelta
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField,SelectField
+from wtforms import StringField, IntegerField, SelectField, BooleanField
 import sqlite3
 import random
 import os
@@ -23,11 +23,12 @@ def main_page():
         exam_name = request.form.get('name')
         exam_duration = request.form.get('duration')
         exam_info = request.form.get('info')
+        show_remaining = request.form.get('show_remaining')
         if False: # add checks
             return render_template('index.html',form=form, error = True)
         exam_starttime = datetime.now().strftime("%H:%M")
         exam_endtime = (datetime.now() + timedelta(minutes=int(exam_duration))).strftime("%H:%M")
-        key = setData(exam_name, exam_starttime, exam_endtime, exam_info)
+        key = setData(exam_name, exam_starttime, exam_endtime, exam_info, show_remaining)
         return redirect(url_for("run", key=key))
     else:
         return render_template('index.html',form=form, error = False)
@@ -36,8 +37,8 @@ def main_page():
 def run(key):
     if getData(key) == False:
         return "Sadly the requested Key can't be found on the server..."
-    (key, exam_name, exam_starttime, exam_endtime, exam_info) = getData(key)
-    return render_template('run.html', exam_name = exam_name, exam_info = exam_info, exam_starttime = exam_starttime, exam_endtime = exam_endtime)
+     (key, exam_name, exam_starttime, exam_endtime, exam_info, show_remaining) = getData(key)
+    return render_template('run.html', exam_name = exam_name, exam_info = exam_info, exam_starttime = exam_starttime, exam_endtime = exam_endtime, show_remaining = show_remaining)
 
 @app.route('/favicon.ico') 
 def favicon(): 
@@ -63,19 +64,20 @@ def makeTable():
                 exam_name text,
                 exam_starttime ,
                 exam_endtime text,
-                exam_info text
+                exam_info text,
+                show_remaining integer
                 )""")
     conn.commit()
     conn.close()
     return
 
-def setData(exam_name, exam_starttime, exam_endtime, exam_info):
+def setData(exam_name, exam_starttime, exam_endtime, exam_info, show_remaining):
     conn = sqlite3.connect(database_location)
     c = conn.cursor()
     key = genKey()
     while isKey(key):
         key = genKey()
-    c.execute("INSERT INTO main VALUES (?, ?, ?, ?, ?)",(key, exam_name, exam_starttime, exam_endtime, exam_info))
+    c.execute("INSERT INTO main VALUES (?, ?, ?, ?, ?, ?)",(key, exam_name, exam_starttime, exam_endtime, exam_info, show_remaining))
     conn.commit()
     conn.close()
     return key
@@ -85,11 +87,11 @@ def getData(key):
     c = conn.cursor()
     c.execute("SELECT * FROM main WHERE key=?", (key, ))
     try:
-        (key, exam_name, exam_starttime, exam_endtime, exam_info) = c.fetchone()
+        (key, exam_name, exam_starttime, exam_endtime, exam_info, show_remaining) = c.fetchone()
     except:
         print("key not found" + key)
         return False
-    return (key, exam_name, exam_starttime, exam_endtime, exam_info)
+    return (key, exam_name, exam_starttime, exam_endtime, exam_info, show_remaining)
 
 def isKey(key):
     conn = sqlite3.connect(database_location)
@@ -109,6 +111,7 @@ class ExamForm(FlaskForm):
     name = StringField('Name der Prüfung: ')
     duration = SelectField('Dauer der Prüfung (in Minuten): ',choices=[60,90,120,150,180])
     info = StringField('Zusätzliche Info (optional): ')
+    show_remaining = BooleanField('Verbleibende Zeit anzeigen?')
 
 if __name__ == '__main__':
     if not checkTable():
